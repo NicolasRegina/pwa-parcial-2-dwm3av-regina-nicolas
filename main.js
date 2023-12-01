7b5941bf8c12044d88fe521e26c495caefeb2a03
NicolasRegina <nicolas.regina@davinci.edu.ar>
2023-12-01 20:57:10 -0300

    Add Firebase a index
    Se agrega firebase al directorio index.html

--- main.js
+++ main.js
@@ -1,3 +1,21 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.7.0/firebase-app.js";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+apiKey: "AIzaSyAISRqC75mWLE-e9tSDwiXkZZDKvQ8kaxQ",
+authDomain: "nintendofirebase.firebaseapp.com",
+projectId: "nintendofirebase",
+storageBucket: "nintendofirebase.appspot.com",
+messagingSenderId: "548790770216",
+appId: "1:548790770216:web:cc7f4e2f66f6314881b31d"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
 //Service Worker
 if ('serviceWorker' in navigator) {
     console.log('Puedes usar los serviceWorker en tu navegador');
